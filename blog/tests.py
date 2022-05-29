7ec07b677e009e00dd6b44a74aa8f01cc1954146
Ahmet Fatih DEVECI <ahmetfatihdeveci@gmail.com>
2022-05-29 22:10:01 +0300

    nc

--- blog/tests.py
+++ blog/tests.py
@@ -8,7 +8,7 @@ from . models import Question, Choice, Post
 class ModelTesting(TestCase):
 
     def setUp(self):
-        self.blog = Post.objects.create("django_test", "fatih", "post")
+        self.blog = Post.objects.create(title = "django_test",author="fatih",slug= "post")
 
     def test_post_model(self):
         d = self.blog

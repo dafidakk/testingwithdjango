2ef3ac3bf51d43f688802e1592d3a6fd7cc37490
Ahmet Fatih DEVECI <ahmetfatihdeveci@gmail.com>
2022-05-29 22:18:40 +0300

    nc

--- blog/tests.py
+++ blog/tests.py
@@ -1,14 +1,14 @@
 from turtle import title
 from unicodedata import name
 from django.test import TestCase
-from . models import Question, Choice, Post 
+from . models import Post 
 # Create your tests here.
 
 
 class ModelTesting(TestCase):
 
     def setUp(self):
-        self.blog = Post.objects.create(title = "django_test",author="fatih",slug= "post")
+        self.blog = Post.objects.create(title ="django_test", author="fatih", slug="post")
 
     def test_post_model(self):
         d = self.blog

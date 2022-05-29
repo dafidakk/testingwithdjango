6b46a82c8a55367235250cb84a3f627e27f65f41
Ahmet Fatih DEVECI <ahmetfatihdeveci@gmail.com>
2022-05-29 21:59:47 +0300

    nc

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,16 @@
+from turtle import title
+from unicodedata import name
 from django.test import TestCase
-
+from . models import Question, Choice, Post 
 # Create your tests here.
+
+
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        self.blog = Post.objects.create("django_test", "fatih", "post")
+
+    def test_post_model(self):
+        d = self.blog
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(d), "django_test")
